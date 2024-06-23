e4ecf498c7220c2dc98f192953f6b22b14b6754b
Subhrangshu Majumder <74654165+subhrangshu9@users.noreply.github.com>
2024-06-23 18:08:18 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,8 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
-export const currency = '₹'
+export const url = 'https://eatswift-backend.onrender.com'
+export const currency = '$'
 
 export const assets ={
     logo,
