0401ee58593417572b662387375cb2689aa817b1
Subhrangshu Majumder <74654165+subhrangshu9@users.noreply.github.com>
2024-06-23 17:11:49 +0530

    Update foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,55 +1,57 @@
 import foodModel from "../models/foodModel.js";
-import fs from 'fs'
+import fs from "fs";
 
-// all food list
-const listFood = async (req, res) => {
-    try {
-        const foods = await foodModel.find({})
-        res.json({ success: true, data: foods })
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
-
-}
-
-// add food
+// Add food item func
+// So whenever addFood api gets clicked, in the body sending these details and access it in the backend using this function
+// Using add food api function, new food items can be added in DB.
 const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
 
-    try {
-        let image_filename = `${req.file.filename}`
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
 
-        const food = new foodModel({
-            name: req.body.name,
-            description: req.body.description,
-            price: req.body.price,
-            category:req.body.category,
-            image: image_filename,
-        })
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error", error: error });
+  }
+};
 
-        await food.save();
-        res.json({ success: true, message: "Food Added" })
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
-}
+// All food list func- so that it can be accessed and send them as response.
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
-// delete food
+// Remove food item func-
 const removeFood = async (req, res) => {
-    try {
+  try {
+    // Find the food model using the id
+    const food = await foodModel.findById(req.body.id);
 
-        const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`, () => { })
+    // removing from file system and uploads folder
+    fs.unlink(`uploads/${food.image}`, () => {});
 
-        await foodModel.findByIdAndDelete(req.body.id)
-        res.json({ success: true, message: "Food Removed" })
+    // removing from DB
+    await foodModel.findByIdAndDelete(req.body.id);
 
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "Error" })
-    }
+    res.json({ success: true, message: "Food Removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
-}
-
-export { listFood, addFood, removeFood }
+export { addFood, listFood, removeFood };
