d656e7212f82e1cbd612fcaa1ff47b06dd1b8835
Subhrangshu Majumder <74654165+subhrangshu9@users.noreply.github.com>
2024-06-23 17:14:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,34 @@
-import express  from "express"
-import cors from 'cors'
-import { connectDB } from "./config/db.js"
-import userRouter from "./routes/userRoute.js"
-import foodRouter from "./routes/foodRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
-// app config
-const app = express()
-const port = process.env.PORT || 4000;
+//app config
+const app = express();
+const port = process.env.PORT || 8000;
 
+//middlewaree
+app.use(cors());
+app.use(express.json());
 
-// middlewares
-app.use(express.json())
-app.use(cors())
+//DB connection
+connectDB();
 
-// db connection
-connectDB()
-
-// api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/cart", cartRouter)
-app.use("/api/order",orderRouter)
+// API endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads")); //access of uploaded images from uploads folder
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API Working")
-  });
+  res.send("API Working");
+});
 
-app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
